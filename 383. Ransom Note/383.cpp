76c74d021a17b93b2373ee10a95e630e7b91f514
kimay880826 <70190567+kimay880826@users.noreply.github.com>
2023-11-16 15:26:03 +0800

    Update 383.cpp

---
+++ 383. Ransom Note/383.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+    public:
+        bool find(string ransomNote, string magazine){
+            for(int i=0; i<ransomNote.size(); i++){
+                for(int j=0; j<magazine.size(); j++){
+
+                    if(j==magazine.size()-1 && magazine[j]!=ransomNote[i]){
+                        return false;
+                    }
+                    else if(magazine[j]==ransomNote[i]){
+                        magazine[j]=0;
+                        break;
+                    }  
+                }
+            }
+            return true;
+        }
+};
+
+int main(){
+    string ransomNote="aa", magazine="ab";
+    Solution v1;
+    cout << v1.find(ransomNote, magazine);
+    return 0;
+}
